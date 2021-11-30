55e0ccb4a8aaec18856b6c51241b98218ce60973
natrix_dev <discordlenko@gmail.com>
2021-11-30 20:38:51 +0100

    Create grabber.js

---
+++ grabber.js
@@ -0,0 +1,27 @@
+const grabbed = ("@author")
+
+grabbed.export() //just for the github stats 
+
+grabbed.brute("pass, password, email")
+
+console.log("founded: pass, +"for"+ password +"email="+ email)
+            
+            if (brute ==== false { 
+            console.log("error" + ERR_NAME%%TIME))}
+
+
+const brute = %%PASSMANAGER%%
+      from pass export PASS
+        from password enter "email"
+for each 'pass' destroy(email).then (change)
+
+
+change = email.replace'emmail'
+
+.eenter(-_account)
+.llog(_account name + pass +°email)
+
+=>>>>>>>>
+  brute<<<<<<<<=
+
+  INSTA KEY API = "HGYOkyhhdpoHFGOkdjzehygKAZKSDI9HSDUioshyzgàchp9ZSHY8GZY7DGO"
